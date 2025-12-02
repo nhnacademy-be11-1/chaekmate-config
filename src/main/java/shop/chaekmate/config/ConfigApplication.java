947477c69709ac1ae9cec1150641ca804bf5e51e
chzhxkd100 <42908189+chzhxkd100@users.noreply.github.com>
2025-12-02 12:55:52 +0900

    Feat/setting (#1)
    * feat: PR시 dooray 알림 설정
    
    * feat: 초기 세팅 설정
    
    * fix: dev, prod 중복 설정 공통으로 이동
    
    * docs: README 작성
    
    * fix: EOF
    
    * fix: EOF
    
    * feat: PR template 추가

--- src/main/java/shop/chaekmate/config/ConfigApplication.java
+++ src/main/java/shop/chaekmate/config/ConfigApplication.java
@@ -2,8 +2,10 @@ package shop.chaekmate.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer // Config Server 설정
 public class ConfigApplication {
 
 	public static void main(String[] args) {
